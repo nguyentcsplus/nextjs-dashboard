8096ecb41f94d61518fda44d74302145b238b8fd
nguyentcsplus <nguyentc@splus-software.com.vn>
2023-12-26 15:48:34 +0700

    Comiit tổng
    Commit1

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,8 +6,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
         haha
-        hihi\
+        hihi
         hehe
+        123
+        456
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
